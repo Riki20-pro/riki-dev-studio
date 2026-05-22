58c9bbedb5620aa1cd9791734f8e2f6673c6afdf
Riki20-pro <rikisiahaan14@gmail.com>
2026-05-22 23:47:11 +0700

    perbaikan ke 2 error deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Required for @vercel/speed-insights/next to work
-  experimental: {
-    instrumentationHook: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
